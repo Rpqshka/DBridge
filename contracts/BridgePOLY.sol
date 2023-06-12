d717a7371516a9a0d70d417ece6b91af8221f233
Alexandr Golikov <rpqshka@gmail.com>
2023-06-12 18:22:31 +0300

    added contracts

---
+++ contracts/BridgePOLY.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IToken.sol";
+
+contract BridgePOLY is Ownable{
+    IToken token;
+    uint256 public nonce;
+    mapping(uint256 => bool) public processedNonces;
+
+    enum Step {
+        Burn,
+        Mint
+    }
+    event Transfer(
+        address from,
+        address to,
+        uint256 amount,
+        uint256 date,
+        uint256 nonce,
+        Step indexed step
+    );
+
+    constructor(address _token) {
+        token = IToken(_token);
+    }
+
+    function burn(address to, uint256 amount) external {
+        token.burn(msg.sender, amount);
+        emit Transfer(
+            msg.sender,
+            to,
+            amount,
+            block.timestamp,
+            nonce,
+            Step.Burn
+        );
+        nonce++;
+    }
+
+    function mint(
+        address to,
+        uint256 amount,
+        uint256 otherChainNonce
+    ) external onlyOwner {
+        require(
+            processedNonces[otherChainNonce] == false,
+            "transfer already processed"
+        );
+        processedNonces[otherChainNonce] = true;
+        token.mint(to, amount);
+        emit Transfer(
+            msg.sender,
+            to,
+            amount,
+            block.timestamp,
+            otherChainNonce,
+            Step.Mint
+        );
+    }
+}
